ff7511433b88856ae908966537e0a7743646eecd
bayu.krisdianto <bayuokta463@gmail.com>
2023-03-20 12:02:05 +0700

    menyelesaikan tantangan Treasure map

---
+++ Day 4 - Randomisation and Python Lists/Exercise/3 - Treasure Map.py
@@ -0,0 +1,13 @@
+row1 = ["0","0","0"]
+row2 = ["0","0","0"]
+row3 = ["0","0","0"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("Where do you want to put the treasure ? ")  #21
+a = int(position[0]) -1
+b = int(position[-1]) -1
+
+map[a][b] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
