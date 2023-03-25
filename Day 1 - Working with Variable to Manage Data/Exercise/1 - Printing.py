1f55040a47bcbda9dd31e5a54b236e0befae317c
bayuo <bayuokta463@gmail.com>
2023-03-25 17:33:28 +0700

    merubah nama folder day 1 dan folder day 2

---
+++ Day 1 - Working with Variable to Manage Data/Exercise/1 - Printing.py
@@ -0,0 +1,3 @@
+print("Day 1 - Working with Variable to Manage Data - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
--- Day 1 - Working with Variable to Manage Data/Exercise/2 - String Manipulation.py
+++ Day 1 - Working with Variable to Manage Data/Exercise/2 - String Manipulation.py
@@ -1,4 +1,4 @@
-print("Day 1 - String Manipulation")
+print("Day 1 - Working with Variable to Manage Data - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
