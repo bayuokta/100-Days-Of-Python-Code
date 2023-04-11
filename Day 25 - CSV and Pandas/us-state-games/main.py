cdd8082f9bf5ec9c61480820558737caf8a84a99
Bayu Krisdianto <bayu.krisdianto@indocyber.id>
2023-04-11 09:42:14 +0700

    menyelesaikan us game state

--- Day 25 - CSV and Pandas/us-state-games/main.py
+++ Day 25 - CSV and Pandas/us-state-games/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S State Game")
@@ -6,4 +7,30 @@ img = "blank_states_img.gif"
 screen.addshape(img)
 turtle.shape(img)
 
-screen.exitonclick()
+data = pandas.read_csv("50_states.csv")
+all_states = data['state'].to_list()
+guest_state = []
+
+while len(guest_state) < 50:
+    answer_state = screen.textinput(title=f'{len(guest_state)} State Correct',
+                                    prompt='What\'s another state\'s name').title()
+
+    if answer_state == 'Exit':
+        break
+    if answer_state in all_states:
+        guest_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data['state'] == answer_state]
+        t.goto(int(state_data['x'].item()), int(state_data['y'].item()))
+        t.write(state_data['state'].item())
+
+
+# states_to_learn.csv
+for state in guest_state:
+    all_states.remove(state)
+
+state_to_learn = pandas.DataFrame(all_states)
+state_to_learn.columns = ['state']
+state_to_learn.to_csv('states_to_learn.csv')
